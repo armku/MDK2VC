9c91c1815826eff2d0d2365ef92f28c0066dfc17
Ace <258613763@qq.com>
2018-01-20 20:15:02 +0800

    优化生成guid

--- Src/MDK2VC/M2V/UI/FormMain.cs
+++ Src/MDK2VC/M2V/UI/FormMain.cs
@@ -207,7 +207,7 @@ namespace MDK2VC
             {
                 var aa = grou.Element("GroupName");
                 builder.Append("    <Filter Include=\"").Append(aa.Value).AppendLine("\">");
-                builder.Append("      <UniqueIdentifier>{").Append("926455ce-06fe-4d2e-a713-9c90bcdea046").AppendLine("}</UniqueIdentifier>");
+                builder.Append("      <UniqueIdentifier>").Append(Guid.NewGuid().ToString("B")).AppendLine("</UniqueIdentifier>");
                 builder.AppendLine("    </Filter>");
             }
         }
