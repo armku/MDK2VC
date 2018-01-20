794aa7eb8d4f744284c662bcc5b91c18148bd1df
Ace <258613763@qq.com>
2018-01-20 19:21:25 +0800

    代码优化 filters

--- Src/MDK2VC/M2V/UI/FormMain.cs
+++ Src/MDK2VC/M2V/UI/FormMain.cs
@@ -322,7 +322,7 @@ namespace MDK2VC
         {
             var builder = new StringBuilder();
 
-            builder.AppendLine("<?xml version=\"1.0\" encoding=\"utf - 8\"?>");
+            builder.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
             builder.AppendLine("<Project ToolsVersion=\"4.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">");
             builder.AppendLine("  <ItemGroup>");
             builder.AppendLine("    <Filter Include=\"CMSIS\">");
