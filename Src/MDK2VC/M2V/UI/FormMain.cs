dfb795b85ff32eec3e4963c272e086dcbd69f19a
Ace <258613763@qq.com>
2018-01-20 15:05:04 +0800

    代码输出改为vc6

--- Src/MDK2VC/M2V/UI/FormMain.cs
+++ Src/MDK2VC/M2V/UI/FormMain.cs
@@ -158,62 +158,160 @@ namespace MDK2VC
             fs.Flush();
             fs.Close();
         }
+        void createdsp(string filename)
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine(@"# Microsoft Developer Studio Project File - Name=""demo"" - Package Owner=<4>");
+            builder.AppendLine(@"# Microsoft Developer Studio Generated Build File, Format Version 6.00");
+            builder.AppendLine(@"# ** DO NOT EDIT **");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# TARGTYPE ""Win32(x86) Application"" 0x0101");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"CFG=demo - Win32 Debug");
+            builder.AppendLine(@"!MESSAGE This is not a valid makefile. To build this project using NMAKE,");
+            builder.AppendLine(@"!MESSAGE use the Export Makefile command and run");
+            builder.AppendLine(@"!MESSAGE ");
+            builder.AppendLine(@"!MESSAGE NMAKE /f ""demo.mak"".");
+            builder.AppendLine(@"!MESSAGE ");
+            builder.AppendLine(@"!MESSAGE You can specify a configuration when running NMAKE");
+            builder.AppendLine(@"!MESSAGE by defining the macro CFG on the command line. For example:");
+            builder.AppendLine(@"!MESSAGE");
+            builder.AppendLine(@"!MESSAGE NMAKE /f ""demo.mak"" CFG=""demo - Win32 Debug""");
+            builder.AppendLine(@"!MESSAGE ");
+            builder.AppendLine(@"!MESSAGE Possible choices for configuration are:");
+            builder.AppendLine(@"!MESSAGE ");
+            builder.AppendLine(@"!MESSAGE ""demo - Win32 Release"" (based on ""Win32(x86) Application"")");
+            builder.AppendLine(@"!MESSAGE ""demo - Win32 Debug"" (based on ""Win32(x86) Application"")");
+            builder.AppendLine(@"!MESSAGE ");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# Begin Project");
+            builder.AppendLine(@"# PROP AllowPerConfigDependencies 0");
+            builder.AppendLine(@"# PROP Scc_ProjName """);
+            builder.AppendLine(@"# PROP Scc_LocalPath """);
+            builder.AppendLine(@"CPP=cl.exe");
+            builder.AppendLine(@"MTL=midl.exe");
+            builder.AppendLine(@"RSC=rc.exe");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"!IF  ""$(CFG)"" == ""demo - Win32 Release""");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# PROP BASE Use_MFC 0");
+            builder.AppendLine(@"# PROP BASE Use_Debug_Libraries 0");
+            builder.AppendLine(@"# PROP BASE Output_Dir ""Release""");
+            builder.AppendLine(@"# PROP BASE Intermediate_Dir ""Release""");
+            builder.AppendLine(@"# PROP BASE Target_Dir """);
+            builder.AppendLine(@"# PROP Use_MFC 0");
+            builder.AppendLine(@"# PROP Use_Debug_Libraries 0");
+            builder.AppendLine(@"# PROP Output_Dir ""Release""");
+            builder.AppendLine(@"# PROP Intermediate_Dir ""Release""");
+            builder.AppendLine(@"# PROP Target_Dir """"");
+            builder.AppendLine(@"# ADD BASE CPP /nologo /W3 /GX /O2 /D ""WIN32"" /D ""NDEBUG"" /D ""_WINDOWS"" /D ""_MBCS"" /YX /FD /c");
+            builder.AppendLine(@"# ADD CPP /nologo /W3 /GX /O2 /D ""WIN32"" /D ""NDEBUG"" /D ""_WINDOWS"" /D ""_MBCS"" /YX /FD /c");
+            builder.AppendLine(@"# ADD BASE MTL /nologo /D ""NDEBUG"" /mktyplib203 /win32");
+            builder.AppendLine(@"# ADD MTL /nologo /D ""NDEBUG"" /mktyplib203 /win32");
+            builder.AppendLine(@"# ADD BASE RSC /l 0x804 /d ""NDEBUG""");
+            builder.AppendLine(@"# ADD RSC /l 0x804 /d ""NDEBUG""");
+            builder.AppendLine(@"BSC32=bscmake.exe");
+            builder.AppendLine(@"# ADD BASE BSC32 /nologo");
+            builder.AppendLine(@"# ADD BSC32 /nologo");
+            builder.AppendLine(@"LINK32=link.exe");
+            builder.AppendLine(@"# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:windows /machine:I386");
+            builder.AppendLine(@"# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:windows /machine:I386");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"!ELSEIF  ""$(CFG)"" == ""demo - Win32 Debug""");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# PROP BASE Use_MFC 0");
+            builder.AppendLine(@"# PROP BASE Use_Debug_Libraries 1");
+            builder.AppendLine(@"# PROP BASE Output_Dir ""Debug""");
+            builder.AppendLine(@"# PROP BASE Intermediate_Dir ""Debug""");
+            builder.AppendLine(@"# PROP BASE Target_Dir """);
+            builder.AppendLine(@"# PROP Use_MFC 0");
+            builder.AppendLine(@"# PROP Use_Debug_Libraries 1");
+            builder.AppendLine(@"# PROP Output_Dir ""Debug""");
+            builder.AppendLine(@"# PROP Intermediate_Dir ""Debug""");
+            builder.AppendLine(@"# PROP Target_Dir """);
+            builder.AppendLine(@"# ADD BASE CPP /nologo /W3 /Gm /GX /ZI /Od /D ""WIN32"" / D ""_DEBUG"" / D ""_WINDOWS"" / D ""_MBCS"" / YX /FD /GZ /c");
+            builder.AppendLine(@"# ADD CPP /nologo /W3 /Gm /GX /ZI /Od /D ""WIN32"" / D ""_DEBUG"" / D ""_WINDOWS"" / D ""_MBCS"" / YX /FD /GZ /c");
+            builder.AppendLine(@"# ADD BASE MTL /nologo /D ""_DEBUG"" / mktyplib203 /win32");
+            builder.AppendLine(@"# ADD MTL /nologo /D ""_DEBUG"" / mktyplib203 /win32");
+            builder.AppendLine(@"# ADD BASE RSC /l 0x804 /d ""_DEBUG""");
+            builder.AppendLine(@"# ADD RSC /l 0x804 /d ""_DEBUG""");
+            builder.AppendLine(@"BSC32 =bscmake.exe");
+            builder.AppendLine(@"# ADD BASE BSC32 /nologo");
+            builder.AppendLine(@"# ADD BSC32 /nologo");
+            builder.AppendLine(@"LINK32=link.exe");
+            builder.AppendLine(@"# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:windows /debug /machine:I386 /pdbtype:sept");
+            builder.AppendLine(@"# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /subsystem:windows /debug /machine:I386 /pdbtype:sept");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"!ENDIF ");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# Begin Target");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# Name ""demo - Win32 Release""");
+            builder.AppendLine(@"# Name ""demo - Win32 Debug""");
+            builder.AppendLine(@"# Begin Group ""Source Files""");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# PROP Default_Filter ""cpp; c; cxx; rc; def; r; odl; idl; hpj; bat""");
+            builder.AppendLine(@"# Begin Source File");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"SOURCE=..\..\..\MCU\STM32\STDOS\STDOS\Core\Array.cpp");
+            builder.AppendLine(@"# End Source File");
+            builder.AppendLine(@"# Begin Source File");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"SOURCE=..\..\..\MCU\STM32\STDOS\STDOS\Core\Buffer.cpp");
+            builder.AppendLine(@"# End Source File");
+            builder.AppendLine(@"# Begin Source File");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"SOURCE=..\..\..\MCU\STM32\STDOS\STDOS\Core\Version.cpp");
+            builder.AppendLine(@"# End Source File");
+            builder.AppendLine(@"# End Group");
+            builder.AppendLine(@"# Begin Group ""Header Files""");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# PROP Default_Filter ""h; hpp; hxx; hm; inl""");
+            builder.AppendLine(@"# Begin Source File");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"SOURCE=..\..\..\MCU\STM32\STDOS\STDOS\Core\_Core.h");
+            builder.AppendLine(@"# End Source File");
+            builder.AppendLine(@"# Begin Source File");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"SOURCE=..\..\..\MCU\STM32\STDOS\STDOS\Core\Type.h");
+            builder.AppendLine(@"# End Source File");
+            builder.AppendLine(@"# Begin Source File");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"SOURCE=..\..\..\MCU\STM32\STDOS\STDOS\Core\Version.h");
+            builder.AppendLine(@"# End Source File");
+            builder.AppendLine(@"# End Group");
+            builder.AppendLine(@"# Begin Group ""Resource Files""");
+            builder.AppendLine(@"");
+            builder.AppendLine(@"# PROP Default_Filter ""ico; cur; bmp; dlg; rc2; rct; bin; rgs; gif; jpg; jpeg; jpe""");
+            builder.AppendLine(@"# End Group");
+            builder.AppendLine(@"# End Target");
+            builder.AppendLine(@"# End Project");
+            builder.AppendLine(@"");
+
+
+
+
+
+
+
+
+
+
+
+
+
+            var fs = new FileStream(filename, FileMode.Create);
+            byte[] data = new UTF8Encoding().GetBytes(builder.ToString());
+            fs.Write(data);
+            fs.Flush();
+            fs.Close();
+        }
 
         private void btnTest_Click(object sender, EventArgs e)
         {
            createdsw(cfg.vcdsw, @".\demo.dsp");
-
-
-
-
-
-            var builderdef = new StringBuilder();
-            getDefine(builderdef);
-
-            
-
-            var builderinclude = new StringBuilder();
-            getIncludePath(builderinclude);
-
-            var xDoc = new XDocument();
-            var Project = new XElement("Project",new XAttribute("DefaultTargets", "Build"));
-            var ItemGroup = new XElement("ItemGroup",new XAttribute("Label", "ProjectConfigurations"));
-
-            var PropertyGroup = new XElement("PropertyGroup", new XAttribute("Label", "Globals"));
-            //var ProjectGuid = new XElement("ProjectGuid",new XAttribute("", @"{DB72F4F2-0882-46C5-83D8-39933DDC3412}"));
-
-            Project.Add(PropertyGroup);
-
-            var ItemDefinitionGroup = new XElement("ItemDefinitionGroup",new XAttribute("Condition", @"'$(Configuration)|$(Platform)'=='Release|x64'"));
-            var ClCompile = new XElement("ClCompile");
-            var AdditionalIncludeDirectories = new XElement("AdditionalIncludeDirectories", builderinclude.ToString());
-            var PreprocessorDefinitions = new XElement("PreprocessorDefinitions", builderdef.ToString()+@";%(PreprocessorDefinitions)");
-
-
-            ClCompile.Add(AdditionalIncludeDirectories);
-            ClCompile.Add(PreprocessorDefinitions);
-
-            ItemDefinitionGroup.Add(ClCompile);
-
-            var ItemGroupfiles = new XElement("ItemGroup");
-            for(int i=0;i<10;i++)
-            {
-                var f1 = new XElement("ClCompile" , new XAttribute("Include", @"..\..\STDOS\App\AT.cpp"));   
-                
-                ItemGroupfiles.Add(f1);
-            }
-
-
-
-            Project.Add(ItemGroup);
-            Project.Add(ItemDefinitionGroup);
-            Project.Add(ItemGroupfiles);
-
-
-            xDoc.Add(Project);
-
-
-            xDoc.Save(cfg.vcdsp);
+            createdsp(cfg.vcdsp);
         }
 
         private void btnOpenDsw_Click(object sender, EventArgs e)
