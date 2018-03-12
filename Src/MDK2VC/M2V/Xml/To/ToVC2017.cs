d20d5afa7ec40f2d141520ee42ab01f615f7a67b
Ace <258613763@qq.com>
2018-03-12 13:43:00 +0800

    代码整理

--- Src/MDK2VC/M2V/Xml/To/ToVC2017.cs
+++ Src/MDK2VC/M2V/Xml/To/ToVC2017.cs
@@ -365,19 +365,19 @@ namespace MDK2VC.M2V.Xml
         {
             var builder = new StringBuilder();
 
+            if (cfg.ProjFiles.Data.Name.StartsWith(".\\"))
+                builder.Append("    <Filter Include=\"").Append(cfg.ProjFiles.Data.Name.Replace(".\\", "..\\")).AppendLine("\">");
+            else
+                builder.Append("    <Filter Include=\"").Append(cfg.ProjFiles.Data.Name).AppendLine("\">");
+            builder.Append("      <UniqueIdentifier>").Append(Guid.NewGuid().ToString("B")).AppendLine("</UniqueIdentifier>");
+            builder.AppendLine("    </Filter>");
+
             if (cfg.ProjFiles.Nodes.Count != 0)
             {
                 for (int i = 0; i < cfg.ProjFiles.Nodes.Count; i++)
                 {
                     var path0 = cfg.ProjFiles.Nodes[i].Data.Name + @"\";
-
-                    if (cfg.ProjFiles.Data.Name.StartsWith(".\\"))
-                        builder.Append("    <Filter Include=\"").Append(cfg.ProjFiles.Data.Name.Replace(".\\", "..\\")).AppendLine("\">");
-                    else
-                        builder.Append("    <Filter Include=\"").Append(cfg.ProjFiles.Data.Name).AppendLine("\">");
-                    builder.Append("      <UniqueIdentifier>").Append(Guid.NewGuid().ToString("B")).AppendLine("</UniqueIdentifier>");
-                    builder.AppendLine("    </Filter>");
-
+                    
                     if (cfg.ProjFiles.Nodes[i].Nodes != null)
                     {
                         if (cfg.ProjFiles.Nodes[i].Data.Name.StartsWith(".\\"))
