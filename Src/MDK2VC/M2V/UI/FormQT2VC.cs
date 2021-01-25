23143c002bf4a62c02cc76e62e571d5bd89853a6
Ace <258613763@qq.com>
2021-01-25 21:18:41 +0800

    优化

--- Src/MDK2VC/M2V/UI/FormQT2VC.cs
+++ Src/MDK2VC/M2V/UI/FormQT2VC.cs
@@ -24,6 +24,27 @@ namespace MDK2VC.M2V.UI
         {
             base.Close();
         }
+        public void Createfilters()
+        {
+            var builder = new StringBuilder();
+
+            builder.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            builder.AppendLine("<Project DefaultTargets=\"Build\" ToolsVersion=\"4.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">");
+            builder.AppendLine("  <ItemGroup>");
+            builder.Append(cfg.ToFilter_files.ToString());
+            //Fromuvprojx.getGrouptoFilters(builder, cfg.MdkPath);
+            builder.AppendLine("  </ItemGroup>");
+            builder.AppendLine("  <ItemGroup>");
+            builder.Append(cfg.ToFilter_FileFolders.ToString());
+            //Fromuvprojx.getGroupsToFilters(builder, cfg.MdkPath);
+            builder.AppendLine("  </ItemGroup>");
+            builder.AppendLine("</Project>");
+            var fs = new FileStream(cfg.Filters, FileMode.Create);
+            byte[] data = new UTF8Encoding().GetBytes(builder.ToString());
+            fs.Write(data);
+            fs.Flush();
+            fs.Close();
+        }
         void tosln()
         {
             var builder = new StringBuilder();
@@ -89,6 +110,168 @@ namespace MDK2VC.M2V.UI
             fs.Flush();
             fs.Close();
         }
+        private String GetMacroDefineVC(string definestr)
+        {
+            var builder = new StringBuilder();
+            builder.Append("      <PreprocessorDefinitions>");
+            builder.Append(definestr).Append("%(PreprocessorDefinitions)</PreprocessorDefinitions>");
+            return builder.ToString();
+        }
+        private String GetIncludePathVC()
+        {
+            var builder = new StringBuilder();
+            builder.Append("      <AdditionalIncludeDirectories>");
+            builder.Append(cfg.IncludePathStr);
+            builder.Append(";%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>");
+            return builder.ToString();
+        }
+        public void Createvcxproj()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            builder.AppendLine("<Project DefaultTargets=\"Build\" ToolsVersion=\"15.0\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">");
+
+            builder.AppendLine("  <ItemGroup Label=\"ProjectConfigurations\">");
+            builder.AppendLine("    <ProjectConfiguration Include=\"Debug | Win32\">");
+            builder.AppendLine("      <Configuration>Debug</Configuration>");
+            builder.AppendLine("      <Platform>Win32</Platform>");
+            builder.AppendLine("    </ProjectConfiguration>");
+            builder.AppendLine("    <ProjectConfiguration Include=\"Release | Win32\">");
+            builder.AppendLine("      <Configuration>Release</Configuration>");
+            builder.AppendLine("      <Platform>Win32</Platform>");
+            builder.AppendLine("    </ProjectConfiguration>");
+            builder.AppendLine("    <ProjectConfiguration Include=\"Debug | x64\">");
+            builder.AppendLine("      <Configuration>Debug</Configuration>");
+            builder.AppendLine("      <Platform>x64</Platform>");
+            builder.AppendLine("    </ProjectConfiguration>");
+            builder.AppendLine("    <ProjectConfiguration Include=\"Release | x64\">");
+            builder.AppendLine("      <Configuration>Release</Configuration>");
+            builder.AppendLine("      <Platform>x64</Platform>");
+            builder.AppendLine("    </ProjectConfiguration>");
+            builder.AppendLine("  </ItemGroup>");
+            builder.AppendLine("  <PropertyGroup Label=\"Globals\">");
+            builder.AppendLine("    <VCProjectVersion>15.0</VCProjectVersion>");
+            builder.Append("    <ProjectGuid>").Append(cfg.Projguidvc).AppendLine("</ProjectGuid>");
+            builder.AppendLine("    <RootNamespace>STM32F1</RootNamespace>");
+            builder.AppendLine("    <WindowsTargetPlatformVersion>10.0</WindowsTargetPlatformVersion>");
+            builder.AppendLine("  </PropertyGroup>");
+            builder.AppendLine("  <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.Default.props\" />");
+            builder.AppendLine("  <PropertyGroup Condition=\"'$(Configuration)|$(Platform)' == 'Debug|Win32'\" Label=\"Configuration\">");
+            builder.AppendLine("    <ConfigurationType>Application</ConfigurationType>");
+            builder.AppendLine("    <UseDebugLibraries>true</UseDebugLibraries>");
+            builder.AppendLine("    <PlatformToolset>v142</PlatformToolset>");
+            builder.AppendLine("    <CharacterSet>MultiByte</CharacterSet>");
+            builder.AppendLine("  </PropertyGroup>");
+            builder.AppendLine("  <PropertyGroup Condition=\"'$(Configuration)|$(Platform)' == 'Release|Win32'\" Label=\"Configuration\">");
+            builder.AppendLine("    <ConfigurationType>Application</ConfigurationType>");
+            builder.AppendLine("    <UseDebugLibraries>false</UseDebugLibraries>");
+            builder.AppendLine("    <PlatformToolset>v142</PlatformToolset>");
+            builder.AppendLine("    <WholeProgramOptimization>true</WholeProgramOptimization>");
+            builder.AppendLine("    <CharacterSet>MultiByte</CharacterSet>");
+            builder.AppendLine("  </PropertyGroup>");
+            builder.AppendLine("  <PropertyGroup Condition=\"'$(Configuration)|$(Platform)' == 'Debug|x64'\" Label=\"Configuration\">");
+            builder.AppendLine("    <ConfigurationType>Application</ConfigurationType>");
+            builder.AppendLine("    <UseDebugLibraries>true</UseDebugLibraries>");
+            builder.AppendLine("    <PlatformToolset>v142</PlatformToolset>");
+            builder.AppendLine("    <CharacterSet>MultiByte</CharacterSet>");
+            builder.AppendLine("  </PropertyGroup>");
+            builder.AppendLine("  <PropertyGroup Condition=\"'$(Configuration)|$(Platform)' == 'Release|x64'\" Label=\"Configuration\">");
+            builder.AppendLine("    <ConfigurationType>Application</ConfigurationType>");
+            builder.AppendLine("    <UseDebugLibraries>false</UseDebugLibraries>");
+            builder.AppendLine("    <PlatformToolset>v142</PlatformToolset>");
+            builder.AppendLine("    <WholeProgramOptimization>true</WholeProgramOptimization>");
+            builder.AppendLine("    <CharacterSet>MultiByte</CharacterSet>");
+            builder.AppendLine("  </PropertyGroup>");
+            builder.AppendLine("  <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.props\" />");
+            builder.AppendLine("  <ImportGroup Label=\"ExtensionSettings\">");
+            builder.AppendLine("  </ImportGroup>");
+            builder.AppendLine("  <ImportGroup Label=\"Shared\">");
+            builder.AppendLine("  </ImportGroup>");
+            builder.AppendLine("  <ImportGroup Label=\"PropertySheets\" Condition=\"'$(Configuration)|$(Platform)' == 'Debug|Win32'\">");
+            builder.AppendLine("    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />");
+            builder.AppendLine("  </ImportGroup>");
+            builder.AppendLine("  <ImportGroup Label=\"PropertySheets\" Condition=\"'$(Configuration)|$(Platform)' == 'Release|Win32'\">");
+            builder.AppendLine("    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />");
+            builder.AppendLine("  </ImportGroup>");
+            builder.AppendLine("  <ImportGroup Label=\"PropertySheets\\\" Condition=\"'$(Configuration)|$(Platform)' == 'Debug|x64'\">");
+            builder.AppendLine("    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />");
+            builder.AppendLine("  </ImportGroup>");
+            builder.AppendLine("  <ImportGroup Label=\"PropertySheets\" Condition=\"'$(Configuration)|$(Platform)' == 'Release|x64'\">");
+            builder.AppendLine("    <Import Project=\"$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props\" Condition=\"exists('$(UserRootDir)\\Microsoft.Cpp.$(Platform).user.props')\" Label=\"LocalAppDataPlatform\" />");
+            builder.AppendLine("  </ImportGroup>");
+            builder.AppendLine("  <PropertyGroup Label=\"UserMacros\" />");
+            builder.AppendLine("  <PropertyGroup />");
+            builder.AppendLine("  <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)' == 'Debug|Win32'\">");
+            builder.AppendLine("    <ClCompile>");
+            builder.AppendLine("      <WarningLevel>Level3</WarningLevel>");
+            builder.AppendLine("      <Optimization>Disabled</Optimization>");
+            builder.AppendLine("      <SDLCheck>true</SDLCheck>");
+            builder.AppendLine("      <ConformanceMode>true</ConformanceMode>");
+            builder.AppendLine(this.GetIncludePathVC());
+            builder.AppendLine(this.GetMacroDefineVC(cfg.MacroDefineStr));
+            builder.AppendLine("    </ClCompile>");
+            builder.AppendLine("  </ItemDefinitionGroup>");
+            builder.AppendLine("  <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)' == 'Debug|x64'\">");
+            builder.AppendLine("    <ClCompile>");
+            builder.AppendLine("      <WarningLevel>Level3</WarningLevel>");
+            builder.AppendLine("      <Optimization>Disabled</Optimization>");
+            builder.AppendLine("      <SDLCheck>true</SDLCheck>");
+            builder.AppendLine("      <ConformanceMode>true</ConformanceMode>");
+            builder.AppendLine(this.GetIncludePathVC());
+            builder.AppendLine(this.GetMacroDefineVC(cfg.MacroDefineStr));
+            builder.AppendLine("    </ClCompile>");
+            builder.AppendLine("  </ItemDefinitionGroup>");
+            builder.AppendLine("  <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)' == 'Release|Win32'\">");
+            builder.AppendLine("    <ClCompile>");
+            builder.AppendLine("      <WarningLevel>Level3</WarningLevel>");
+            builder.AppendLine("      <Optimization>MaxSpeed</Optimization>");
+            builder.AppendLine("      <FunctionLevelLinking>true</FunctionLevelLinking>");
+            builder.AppendLine("      <IntrinsicFunctions>true</IntrinsicFunctions>");
+            builder.AppendLine("      <SDLCheck>true</SDLCheck>");
+            builder.AppendLine("      <ConformanceMode>true</ConformanceMode>");
+            builder.AppendLine(this.GetIncludePathVC());
+            builder.AppendLine(this.GetMacroDefineVC(cfg.MacroDefineStr));
+            builder.AppendLine("    </ClCompile>");
+            builder.AppendLine("    <Link>");
+            builder.AppendLine("      <EnableCOMDATFolding>true</EnableCOMDATFolding>");
+            builder.AppendLine("      <OptimizeReferences>true</OptimizeReferences>");
+            builder.AppendLine("    </Link>");
+            builder.AppendLine("  </ItemDefinitionGroup>");
+            builder.AppendLine("  <ItemDefinitionGroup Condition=\"'$(Configuration)|$(Platform)' == 'Release|x64'\">");
+            builder.AppendLine("    <ClCompile>");
+            builder.AppendLine("      <WarningLevel>Level3</WarningLevel>");
+            builder.AppendLine("      <Optimization>MaxSpeed</Optimization>");
+            builder.AppendLine("      <FunctionLevelLinking>true</FunctionLevelLinking>");
+            builder.AppendLine("      <IntrinsicFunctions>true</IntrinsicFunctions>");
+            builder.AppendLine("      <SDLCheck>true</SDLCheck>");
+            builder.AppendLine("      <ConformanceMode>true</ConformanceMode>");
+            builder.AppendLine(this.GetIncludePathVC());
+            builder.AppendLine(this.GetMacroDefineVC(cfg.MacroDefineStr));
+            builder.AppendLine("    </ClCompile>");
+            builder.AppendLine(@"    <Link>");
+            builder.AppendLine(@"      <EnableCOMDATFolding>true</EnableCOMDATFolding>");
+            builder.AppendLine(@"      <OptimizeReferences>true</OptimizeReferences>");
+            builder.AppendLine(@"    </Link>");
+            builder.AppendLine(@"  </ItemDefinitionGroup>");
+            builder.AppendLine(@"  <ItemGroup>");
+            //Fromuvprojx.getGroupsToProj(builder, cfg.MdkPath);
+            builder.Append(cfg.ToProj_Files.ToString());
+            builder.AppendLine(@"  </ItemGroup>");
+            builder.AppendLine("  <Import Project=\"$(VCTargetsPath)\\Microsoft.Cpp.targets\" />");
+            builder.AppendLine("  <ImportGroup Label=\"ExtensionTargets\">");
+            builder.AppendLine(@"  </ImportGroup>");
+            builder.AppendLine(@"</Project>");
+
+            if (!Directory.Exists(cfg.VCPath))
+            {
+                Directory.CreateDirectory(cfg.VCPath);
+            }
+            var fs = new FileStream(cfg.Vcxproj, FileMode.Create);
+            byte[] data = new UTF8Encoding().GetBytes(builder.ToString());
+            fs.Write(data);
+            fs.Flush();
+            fs.Close();
+        }
         private void btnTest_Click(object sender, EventArgs e)
         {
             cfg.Sln = MDK2VCConfig.Current.StrQTVsFilePath;
@@ -101,6 +284,8 @@ namespace MDK2VC.M2V.UI
             }
 
             tosln();
+            Createvcxproj();
+            Createfilters();
 
         }
         string GetVsPath(string str)
@@ -140,5 +325,14 @@ namespace MDK2VC.M2V.UI
         public string FileNameWithoutExtension;
         public string Projguidvc;
         public string Sln;
+
+        public string Filters;
+        public string ToFilter_FileFolders;
+        public string ToFilter_files;
+        public string ToProj_Files;
+        public string VCPath;
+        public string Vcxproj;
+        public string MacroDefineStr;
+        public string IncludePathStr;
     }
 }
